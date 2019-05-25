6bbe9d7a0ee341bb286e22e5d8e9ef6f7f3d9e23
NurseyitTursunkulov <nurseyit.tursunkulov@gmail.com>
2019-05-25 22:51:36 +0600

    changed asynkoin to retrofit

---
+++ app/src/test/java/com/example/sonyadmin/FakeRepository.kt
@@ -0,0 +1,69 @@
+package com.example.sonyadmin
+
+import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
+import com.example.sonyadmin.data.DailyCount
+import com.example.sonyadmin.data.Price
+import com.example.sonyadmin.data.Repository
+import com.example.sonyadmin.data.Task
+import org.joda.time.DateTime
+
+class FakeRepository : Repository{
+    override fun setPrice(price: Price) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getPrice(): LiveData<Price> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override var list: ArrayList<LiveData<Task>>
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        set(value) {}
+    override val first: LiveData<Task>
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+    override fun writeStartTime(game: Task) {
+    }
+
+    override fun writeEndTime(game: Task) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getAllGameProccesBiCabin(cabinId: Int): LiveData<List<Task>>? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getLastGame(cabinId: Int): Task {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getLastGameLive(cabinId: Int): LiveData<Task> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun deleteAll() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setCash(dailyCount: DailyCount) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun updateCash(sTimeOfDay: DateTime, endTimeOfDay: DateTime, sum: Double) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCash(): DataSource.Factory<Int, DailyCount> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getGameDetails(cabinId: Int): DataSource.Factory<Int, Task> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getLastCash(): DailyCount? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
