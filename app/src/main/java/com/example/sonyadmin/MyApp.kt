078d4ab200d5255885420036c39040040062e341
NurseyitTursunkulov <nurseyit.tursunkulov@gmail.com>
2019-05-25 23:03:29 +0600

    changed asynkoin to retrofit

--- app/src/main/java/com/example/sonyadmin/MyApp.kt
+++ app/src/main/java/com/example/sonyadmin/MyApp.kt
@@ -31,6 +31,7 @@ import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class MyApp : Application(){
     override fun onCreate() {
@@ -65,12 +66,15 @@ val appModule = module {
             .setLevel(HttpLoggingInterceptor.Level.HEADERS)
     }
 
-    factory { OkHttpClient.Builder().addInterceptor(get()).build() }
+    factory { OkHttpClient.Builder().addInterceptor(get())
+        .connectTimeout(5, TimeUnit.SECONDS)
+        .readTimeout(5, TimeUnit.SECONDS)
+        .build() }
 
     single {
         Retrofit.Builder()
             .client(get())
-            .baseUrl("http://192.168.43.9:5000/")
+            .baseUrl("http://jsonplaceholder.typicode.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .build()
--- app/src/main/java/com/example/sonyadmin/gameList/MyModel.kt
+++ app/src/main/java/com/example/sonyadmin/gameList/MyModel.kt
@@ -15,15 +15,17 @@ import com.example.sonyadmin.Event
 import com.example.sonyadmin.MyCoroutineWorker
 import com.example.sonyadmin.data.Repository
 import com.example.sonyadmin.data.Task
+import com.example.sonyadmin.data.service.PlaceholderPosts
 import com.example.sonyadmin.data.service.UserRepository
 import com.example.sonyadmin.gameList.Model.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import me.nikhilchaudhari.asynkio.core.async
 import me.nikhilchaudhari.asynkio.core.request
 import me.nikhilchaudhari.asynkio.response.Response
 import org.joda.time.DateTime
+import java.lang.Exception
+import java.net.ConnectException
+import java.net.SocketTimeoutException
 
 class MyModel(var repository: Repository, application: Application, val userRepository: UserRepository) :
     AndroidViewModel(application),
@@ -32,7 +34,7 @@ class MyModel(var repository: Repository, application: Application, val userRepo
         return viewModelScope
     }
 
-    private val _showToast = MutableLiveData<Event<String>>()
+    internal val _showToast = MutableLiveData<Event<String>>()
 
     val showToast: LiveData<Event<String>>
         get() = _showToast
@@ -40,9 +42,8 @@ class MyModel(var repository: Repository, application: Application, val userRepo
     val items = MutableLiveData<List<LiveData<Task>>>().apply {
         value = emptyList()
     }
-    val liveItems =repository.list
+    val liveItems = repository.list
     val dataLoading = MutableLiveData<Boolean>(false)
-    val anyData = MutableLiveData<String>("hz")
 
     init {
         liveItems.forEach {
@@ -58,33 +59,25 @@ class MyModel(var repository: Repository, application: Application, val userRepo
     }
 
     fun completeTask(task: Task) {
-//        launch {
-//            val h = userRepository.search()
-//            if (h.isSuccessful)
-//            Log.d("Retrofit","eeeeee Retrofit ${h}")
-//        }
-        sendRequest(task, dataLoading,"off", _showToast) {
+        makeRequest( { userRepository.search() }, {
             repository.writeEndTime(changeGameEndTime(task))
-            repository.updateCash( DateTime.now().withTime(0, 0, 0, 0), DateTime.now().withTime(23, 59, 59, 0),
+            repository.updateCash(
+                DateTime.now().withTime(0, 0, 0, 0), DateTime.now().withTime(23, 59, 59, 0),
                 countSum(task, DateTime.now())
             )
-        }
+        })
+
     }
 
 
-    fun openTask(task: Task) =viewModelScope.launch{
-//            val h = userRepository.search().body()
-//        repository.writeStartTime(changeGameStartTime(task))
-        sendRequest(task, dataLoading,"onn", _showToast) { repository.writeStartTime(changeGameStartTime(task)) }
+    fun openTask(task: Task)  {
+        makeRequest( {userRepository.onn()}, {
+            repository.writeStartTime(changeGameStartTime(task))
+        })
+
     }
 
     fun deleteAll() {
         repository.deleteAll()
-    }
-
-    fun isInternetOn(context: Context): Boolean {
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
-        val activeNetwork = cm?.activeNetworkInfo
-        return activeNetwork != null && activeNetwork.isConnected
     }
 }
